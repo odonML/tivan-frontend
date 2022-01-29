e6540238fc12a533e0f4cee28ed4ed765380c173
odonML <odon_ml@outlook.com>
2022-01-28 20:03:10 -0600

    remove classnames

--- src/components/shared/ButtonText.js
+++ src/components/shared/ButtonText.js
@@ -4,13 +4,14 @@ function ButtonText({
   id = null,
   children = null,
   type = true,
+  hover = true,
+  primary = true,
   flex = "flex",
   flexDirection = "flex-row",
   alingItem = "items-center",
   justifyContent = "justify-center",
   radio = "rounded-full",
   fontSize = "text-base",
-  color = "bg-purple-0",
   txColor = "text-white",
   transitionTaiming = "ease-out",
   transitionDuration = "duration-300",
@@ -26,7 +27,19 @@ function ButtonText({
       type={type ? "button" : "submit"}
       className={`w-full h-fit m-1 shadow hover:shadow-md md:text-md sm:text-sm hover:outline-1 hover:bg-transparent
       ${flex} ${flexDirection} ${alingItem} ${justifyContent} ${radio} 
-      ${txColor} ${fontSize} ${transitionTaiming} ${transitionDuration} ${color}`}
+      ${txColor} ${fontSize} ${transitionTaiming} ${transitionDuration} ${
+        primary
+          ? `bg-purple-0 ${
+              hover
+                ? "hover:text-purple-0 hover:outline hover:outline-purple-0 hover:outline-offset-0 hover:shadow-purple-0/40"
+                : ""
+            }`
+          : `bg-pink-0 ${
+              hover
+                ? "hover:text-pink-0 hover:outline hover:outline-pink-0 hover:outline-offset-0 hover:shadow-pink-0/40"
+                : ""
+            }`
+      }`}
       onClick={(e) => handleClick(e)}
     >
       {children}
