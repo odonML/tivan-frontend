d378403fc944e0e6a7996cb59ba103494810c0ec
odonML <odon_ml@outlook.com>
2022-02-03 16:28:15 -0600

    layout

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Navbar({ children }) {
   return (
-    <nav className="w-full h-full px-1 py-2 bg-purple-0 flex items-center justify-between md:justify-start ">
+    <nav className="w-full h-14 px-1 py-2 bg-purple-0 flex items-center justify-between md:justify-start md:h-10 ">
       {children}
     </nav>
   );
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,12 +1,41 @@
+import ButtonIcon from "components/shared/ButtonIcon";
+import Container from "components/shared/Container";
+import Logo from "components/shared/Logo";
+import Navbar from "components/shared/Navbar";
+import Sidebar from "components/shared/Sidebar";
 import React from "react";
+import { BiSearchAlt2 } from "react-icons/bi";
 import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
-    <div className="">
-      <h1>Layout</h1>
-      <Outlet />
-    </div>
+    <Container height="h-screen" flexDirection="flex-col">
+      <div className="w-full ">
+        <Navbar>
+          <Logo>Tivan</Logo>
+          <div className="block md:hidden">
+            <ButtonIcon icon={<BiSearchAlt2 />} />
+          </div>
+          <div className="hidden md:block">
+            <input
+              className="rounded-full px-3"
+              type="text"
+              placeholder="Search..."
+            />
+          </div>
+        </Navbar>
+      </div>
+      <Container
+        height="h-full"
+        flexDirection="flex-col-reverse"
+        mediaQueries="md:flex-row"
+      >
+        <Sidebar></Sidebar>
+        <Container>
+          <Outlet />
+        </Container>
+      </Container>
+    </Container>
   );
 }
 
