e8ed36994096b06bc448f4a83de2d2390dcbd9fe
EXIDEOR <elfego.aguilar@alumnos.udg.mx>
2022-02-08 14:20:59 -0600

    reorganization of page sales with grids

---
+++ src/components/sub-pages/TicketDetails.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Content from "../shared/Content";
+import ContentTitle from "../shared/ContentTitle";
+
+function ShoppingCar() {
+  return (
+    <Content>
+      <ContentTitle>
+        <p>TICKET</p>
+      </ContentTitle>
+      <div className="w-full h-[85%]"></div>
+    </Content>
+  );
+}
+
+export default ShoppingCar;
--- src/pages/Sales.js
+++ src/pages/Sales.js
@@ -1,32 +1,65 @@
 import React, { useState } from "react";
-import TitleSection from "../components/shared/TitleSection";
+import { AiOutlinePlus } from "react-icons/ai";
+import ContentTitle from "../components/shared/ContentTitle";
+import Content from "../components/shared/Content";
 import CardTicket from "../components/CardTicket/CardTicket";
+import ContentGrid from "../components/shared/ContentGrid";
+import ButtonIcon from "../components/shared/ButtonIcon";
+import TicketDetails from "../components/sub-pages/TicketDetails";
 
 function Sales() {
-  const [tab] = useState(1);
+  const [tab, setTab] = useState(1);
 
   return (
-    <div className="relative bg-gray-3 w-full h-full grid grid-cols-1 grid-rows-1 sm:grid-cols-3 sm:grid-rows-1 p-1">
-      <div
-        className={`${
-          tab === 1 ? "grid" : "hidden"
-        } sm:grid col-span-1 row-span-1 sm:col-span-2 sm:row-span-2 m-2 gap-2 h-0`}
-      >
-        <div className="flex">
-          <TitleSection text="Ventas" locked={true} />
+    <Content>
+      <ContentGrid>
+        <div
+          className={`${
+            tab === 1 ? "grid" : "hidden"
+          } sm:grid col-span-1 row-span-1 md:col-span-4 md:row-span-4 h-1 m-2 p-2 gap-2`}
+        >
+          <ContentTitle>
+            <p>Ventas</p>
+            <ButtonIcon icon={<AiOutlinePlus size={22} />} />
+          </ContentTitle>
+          <CardTicket />
+          <CardTicket />
         </div>
-        <CardTicket />
-        <CardTicket />
-        <CardTicket />
-      </div>
-      <div
-        className={`${
-          tab === 2 ? "grid col-span-1 row-span-1" : "hidden"
-        } sm:grid sm:row-span-4 border`}
-      >
-        <p>Ticket</p>
-      </div>
-    </div>
+        <div
+          className={`${
+            tab === 2 ? "grid col-span-2 row-span-1" : "hidden"
+          } md:grid md:col-span-2 md:row-span-4`}
+        >
+          <TicketDetails className="bg-white" />
+        </div>
+        <div className="absolute bottom-0 w-full h-7 px-1 flex justify-center md:hidden">
+          <div className="flex w-2/3 text-white text-base">
+            <div className="w-1/2">
+              <button
+                type="button"
+                className={`w-full rounded-l-full ${
+                  tab === 1 ? "bg-purple-0" : "bg-pink-0"
+                }`}
+                onClick={() => setTab(1)}
+              >
+                Favoritos
+              </button>
+            </div>
+            <div className="w-1/2">
+              <button
+                type="button"
+                className={`w-full rounded-r-full ${
+                  tab === 2 ? "bg-purple-0" : "bg-pink-0"
+                }`}
+                onClick={() => setTab(2)}
+              >
+                Carrito
+              </button>
+            </div>
+          </div>
+        </div>
+      </ContentGrid>
+    </Content>
   );
 }
 
