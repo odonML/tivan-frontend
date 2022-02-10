2d4ac969c0effe9874b60c5656d9c4a57b35ad08
EXIDEOR <elfego.aguilar@alumnos.udg.mx>
2022-02-09 21:16:49 -0600

    edit component suplycards

--- src/components/sub-pages/SuplyCards.js
+++ src/components/sub-pages/SuplyCards.js
@@ -6,7 +6,10 @@ function SuplyCards() {
   return (
     <Content>
       <div className="w-full h-[100%] p-3 grid grid-cols-1 sm:grid-cols-4 gap-3 overflow-y-scroll custom-scroll">
-        <div className="flex flex-wrap p-2 col-span-1 h-36 max-h-full">
+        <div>
+          <CardSuply />
+        </div>
+        <div>
           <CardSuply />
         </div>
       </div>
