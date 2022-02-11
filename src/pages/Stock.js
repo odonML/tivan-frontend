668d3807f764f7cc4db1945776b41360243112cd
odonML <odon_ml@outlook.com>
2022-02-10 19:56:38 -0600

    demo one field print data of product in form

--- src/pages/Stock.js
+++ src/pages/Stock.js
@@ -3,17 +3,28 @@ import ButtonIcon from "components/shared/ButtonIcon";
 import ContentGrid from "components/shared/ContentGrid";
 import ContentLeft from "components/shared/ContentLeft";
 import ContentRight from "components/shared/ContentRight";
-import React from "react";
+import React, { useState } from "react";
 import FormProduct from "../components/Form/FormProduct";
 
 function Stock() {
+  const [dataProduct, setDataProduct] = useState();
+  const products = [
+    {
+      name: "Sabritas",
+    },
+  ];
   return (
     <ContentGrid>
       <div className="grid md:grid col-span-1 row-span-1 md:col-span-3 lg:col-span-4 md:row-span-4 border border-pink-0">
         <ContentLeft title="Inventario" element={<ButtonIcon />}>
-          <div className="col-span-1 md:col-span-2 lg:col-span-1 h-36 sm:h-24">
-            <CardProduct />
-          </div>
+          {products.map((product) => (
+            <div className="col-span-1 md:col-span-2 lg:col-span-1 h-36 sm:h-24">
+              <CardProduct
+                nameProduct={product.name}
+                clickCard={() => setDataProduct(product)}
+              />
+            </div>
+          ))}
         </ContentLeft>
       </div>
 
@@ -21,6 +32,7 @@ function Stock() {
         <ContentRight title="Detalles">
           <div className="col-span-1 h-full">
             <FormProduct
+              dataProduct={dataProduct}
               click={(data) => console.log("padre", data)}
             ></FormProduct>
           </div>
