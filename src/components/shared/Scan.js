4bd28237400e9f0b9ce63b1898559519d03074a7
odonML <odon_ml@outlook.com>
2022-02-17 22:54:33 -0600

    scandit start

---
+++ src/components/shared/Scan.js
@@ -0,0 +1,52 @@
+import ScanerPicker from "components/ScanerPicker/ScanerPicker";
+import React, { useState } from "react";
+import { ScanSettings } from "scandit-sdk";
+
+function Scan() {
+  const [code, setCode] = useState();
+
+  // configuracion de deteccion del scaner
+  const configScan = {
+    enabledSymbologies: [
+      "qr",
+      "ean8",
+      "ean13",
+      "upca",
+      "upce",
+      "code128",
+      "code39",
+      "code93",
+      "itf",
+    ],
+    codeDuplicateFilter: 1000,
+  };
+  // cuando se logre escaner de manera correcta pasara esot
+  const runScaner = (scanResult) => {
+    console.log("resultados", scanResult.barcodes);
+    const scaningDetected = scanResult.barcodes.reduce(
+      (string, barcode) => barcode.data,
+      ""
+    );
+    setCode(scaningDetected);
+  };
+  // cuando ocurra un erro en el escaner pasara esto
+  const errorScaner = (error) => {
+    console.error(error.message);
+  };
+
+  // objeto porp para el ScannerPicker
+  const scanProps = {
+    playSoundOnScan: true,
+    vibrateOnScan: false,
+    scanSettings: new ScanSettings(configScan),
+    onScan: runScaner,
+    onError: errorScaner,
+  };
+  return (
+    <div className="w-[50%]">
+      <ScanerPicker scanProps={scanProps} code={code} />
+    </div>
+  );
+}
+
+export default Scan;
