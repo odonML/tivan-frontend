251615940cbe004282b87766df871fe9f0064a40
odonML <odon_ml@outlook.com>
2022-02-24 15:36:29 -0600

    new api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 // se tiene que sustituir por la URL
-const API_URL = "https://tivan.auroraescalera.net/api/";
-// "http://localhost:8081/api/";
+const API_URL = "https://tivandb.auroraescalera.net/api/";
+//"http://localhost:8081/api/";
 
 export { API_URL };
