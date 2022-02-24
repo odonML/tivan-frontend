f0849b23c1101b3e6c4934acc0102bc4a1c77f64
odonML <odon_ml@outlook.com>
2022-02-24 12:25:37 -0600

    add new key + domain

--- src/components/ScanerPicker/ScanerPicker.js
+++ src/components/ScanerPicker/ScanerPicker.js
@@ -3,7 +3,7 @@ import {
   BarcodePicker as ScanditSDKBarcodePicker,
   configure,
 } from "scandit-sdk";
-
+// new Key : ---------------------------
 // Configuracion con api key
 const configScan = configure(
   "AeeBT2OkQgLbIHvhNwj1bu49buFLCauP3QsLohd++zWoDeii33Rr5iIPrwodfRZ8pFkRLilZJeYjVq3SUEe4fnB4MaH2bH+gPXCdXQ9WHF2uZp7Qsn09BfFvwlNIUon6mnTtk+50cO8QU6O4+lFhOHVYQbRHKP8Qigo3Geh+mlQ/cuYS0DJdFX1PpHt/QGYPcyZsrIoLlzGTNcP+ng2O/PlFWAZeKKwq+HRyl2Z156adKoi5drfoh0Y8l3CRCQsmAktjjEfO+jxo5lHIdFZw5oLOUmL3FaAiRkuNOaDkbccL+a6RmDCh4MvYff+RwWdZ+GZALNmx0DiNzs5av3XZubwhMbGhD+hRt4r9W49nCOvUYf0XMDu1NiOhV7IAZe9CJexQLMd6t/wz2ENpZRXCWk7wOdXjGeICW31ShmXrKWBPjdn8C0/nyiYxYSo8u40QLIC/VhtgMr8FHSzzvAOPWbadX5oWdDUmbHpqDmFDkkvl2V7Qtqo/99ySWS12xKdLlW0//slZk8BFGOOPR7LOcwdErilrPsOkPGHqk9NYu5hJScR7y6hdNdJBUGGOs9AwDsREH8J5Ntz6t2hqP5nfpDjp668jfOFNEcUWWvVP5EeUqrfuzAKti045Z/KehgkEz7b0sfIKJhRo84GS89dHrNnJsKUc3urhy6R6Y07SUTSo0ZL016d8waNhf88xgk0Mflz66s5cOxgBhkfI/tk+czHC+6xnj5VAOGo9lF7tk/jI/u28lDSLsYdlPnwNDDpfnqCWkX5pN1+Eyvbi41pricWYmt4lxy5BbT161NnDksX6aNKx7Pb/85fw/mZjMBclkmvz+IYj13XsATyRrp7OSecrFURMqR13R1+U673HenIGnGjWtDKmZO9IviWD/aHX8QLaFVtXP5gXYvvuyaYxM6aZ0Q==",
